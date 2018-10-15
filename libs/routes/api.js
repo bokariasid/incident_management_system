81fae4dd38e21c8c0cd49eb798ed977a04a445d4
bokariasid <bokariasiddhartha@gmail.com>
2018-10-16 01:22:40 +0530

    api date added

--- libs/routes/api.js
+++ libs/routes/api.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
-
+var moment = require('moment');
 /* GET users listing. */
 router.get('/', function (req, res) {
+    let dateString = moment().format('YYYY-MM-DD HH:m:ss');
     res.json({
-    	msg: 'API is running'
+    	msg: 'API is running as on '+dateString
     });
 });
 
